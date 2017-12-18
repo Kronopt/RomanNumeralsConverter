d679381401dd9cdedab706004ac9dbfc293137f3
Kronopt <Kronopt@users.noreply.github.com>
2017-12-18 05:10:41 +0000

    Tests skeleton

---
+++ tests/test_RomanNumeralsConverter.py
@@ -0,0 +1,22 @@
+#!python2
+# coding: utf-8
+
+"""
+Tests for RomanNumeralsConverter.py
+"""
+
+
+import unittest
+
+
+class TestSomething(unittest.TestCase):  # TODO
+
+    def setUp(self):
+        pass  # TODO, some setup, if needed
+
+    def test_case_1(self):
+        pass  # TODO, self.assertEquals, etc
+
+
+class TestSomething2(unittest.TestCase):  # TODO
+    pass
